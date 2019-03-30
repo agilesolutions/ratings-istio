57088fcde1e7fd09eb3d828b0d09eaf74b926df0
Robert Rong <robert.rong@agile-solutions.ch>
2019-03-30 10:36:30 +0100

    init

---
+++ src/test/java/sh/agilesolutions/quarkus/NativeRatingsResourceIT.java
@@ -0,0 +1,9 @@
+package sh.agilesolutions.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeRatingsResourceIT extends RatingsResourceTest {
+
+    // Execute the same tests but in native mode.
+}
